a6e6bddc56cd33fff372aab8e3391f036f2f6f0d
MLHaddon <haddon.mike2@outlook.com>
2021-04-16 02:45:07 -0500

    Added Image Gallery Core Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import $ from 'jquery';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import headerLogo from './static/BAG_LOGO.png';
-import 'bootstrap/dist/'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function App() {
+  const [images, setImages] = useState([]);
+
   useEffect(() => {
     if ($) {
       $('nav li').hover(
@@ -18,28 +21,51 @@ function App() {
     }
   }, [$]);
 
+  const handleRequest = e => {
+    let randWidth = Math.round(Math.random() * (500 - 100) + 100);
+    let randHeight = Math.round(Math.random() * (500 - 100) + 100);
+    axios.get(`https://picsum.photos/${randWidth}/${randHeight}`)
+      .then(res => {
+        setImages(oldImgs => [...oldImgs, res.config.url]);
+        console.log(res.config.url);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   return (
     <div className="App">
       <header>
-        <nav className="d-flex flex-row justify-content-between">
-          <div className="container">
-            <ul>
-              <li><a href="#">|||</a>
-                <ul>
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Artists</a></li>
-                  <li><a href="#">Store</a></li>
-                  <li><a href="#">Blog</a></li>
-                </ul>
-              </li>
-            </ul>
-            <img src={headerLogo} alt="Loading..." />
-          </div>
+        <nav className="navbar p-4 d-flex flex-row justify-content-between align-items-center">
+          <ul>
+            <li><a href="#">|||</a>
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Artists</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">Blog</a></li>
+              </ul>
+            </li>
+          </ul>
+          <img src={headerLogo} alt="Loading..." id="logo" />
         </nav>
       </header>
-      <body>
-      </body>
+      <div className="d-flex justify-content-center">
+        <button className="btn btn-success" onClick={handleRequest}>Add Image</button>
+      </div>
+        <div className="w-100 d-flex flex-wrap">
+          <div className="container">
+            <section id="photos">
+            {
+              images.map((val, idx) => 
+                <img src={val} alt="Loading..." key={idx} />
+              )
+            }
+            </section>
+          </div>
+        </div>
     </div>
   );
 }
